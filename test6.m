c8142e919799c59b576b045282983d1a5d08fa9c
xubo888 <xubo@example.com>
2023-12-19 15:43:43 +0800

    xubo v3

---
+++ test6.m
@@ -0,0 +1,14 @@
+clear all
+n = 3; k = 2;               % A (3,2) 循环码
+N=10000;                    %消息比特的行数           
+msg = randint(N,k,2,123);         %消息比特共N*k行
+% pol=cyclpoly(n,k);          %循环码的生成多项式
+% [h,g]=cyclgen(n,pol);       %生成循环码
+% code2 = mod(msg*g,2);
+code2 =encode(msg,n,k,'cyclic')
+
+noisy=randerr(N,n,[0 1;0.7 0.3]);           %噪声
+noisycode2 = mod(code2 + noisy,2);      %加入噪声
+newmsg2 = decode(noisycode2,n,k,'cyclic'); % 译码.
+ [number,ratio2] = biterr(newmsg2,msg); %误比特率
+disp(['The bit error rate2 is ',num2str(ratio2)])
